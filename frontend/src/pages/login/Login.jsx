135d2e2c930a9d9c8b1d593b3e9a4e4667efbfa6
samieruashovo <samieruashovo@gmail.com>
2024-06-16 17:34:42 +0600

    4th commit

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -15,10 +15,9 @@ const Login = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
-      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+      <div className="w-full bg-purple-500 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10">
         <h1 className="text-3xl font-semibold text-center text-gray-300">
-          Login
-          <span className="text-blue-500"> ChatApp</span>
+          <span className="text-blue-500">Login</span>
         </h1>
 
         <form onSubmit={handleSubmit}>
